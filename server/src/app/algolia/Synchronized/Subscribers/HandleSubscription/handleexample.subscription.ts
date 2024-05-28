fcc8feb4484b4dbcc4d72a1c4d66783f06f411cf
tinict <tinnguyensp.ict@gmail.com>
2024-05-28 15:20:23 +0700

    [main]: Remove synchonized of zealsync

--- server/src/app/algolia/Synchronized/Subscribers/HandleSubscription/handleexample.subscription.ts
+++
@@ -1,5 +0,0 @@
-export default function OrderHandelSubcription (message: any, channel: any) {
-    console.log(new Date().getTime());
-    console.log("Channel", channel);
-    console.log("Message", JSON.parse(message));
-};
